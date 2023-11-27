b1569551fe5fcb54d2efb3a4a6e776844acc9e20
YellowDuck48 <152202684+YellowDuck48@users.noreply.github.com>
2023-11-27 19:41:00 +0300

    утка

---
+++ HalloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //вывод в консоль приветствия
+        print("HelloWorld"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
